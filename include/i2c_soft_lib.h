73f1b577bf525460b2fdc665252621c0adf71b12
Lumenjer <lumenjer@gmail.com>
2022-12-18 01:02:36 +0200

    Add .cpp file, update header

--- include/i2c_soft_lib.h
+++ include/i2c_soft_lib.h
@@ -7,12 +7,20 @@
     #define DEVICE_MAX 10
 #endif
 
+#ifndef ASSERT
+  #define ASSERT(x) \
+    ((x) \
+        ? (void)0 \
+        : (void)0\
+    )
+#endif
+
 typedef bool(*read_pin_cb)(void);
 
 typedef struct {
   read_pin_cb read_sda_ptr;
   read_pin_cb read_scl_ptr;
-} i2c_soft_init;
+} i2c_soft_init_struct;
 
 typedef enum {
   I2C_SOFT_DISPLAY = 0,
@@ -31,7 +39,7 @@ typedef enum {
 
 typedef struct {
   i2c_soft_devtype type;
-  uint8_t addr;    
+  uint8_t addr;
 } i2c_soft_device;
 
 typedef struct {
@@ -40,11 +48,11 @@ typedef struct {
   i2c_soft_device device_arr[DEVICE_MAX];
 } i2c_soft_bus;
 
-void i2C_soft_init(i2c_soft_init* init_ptr);
+void i2c_soft_init(i2c_soft_bus* bus_ptr, i2c_soft_init_struct* init_ptr);
 
 void i2C_soft_handler(i2c_soft_bus* bus_ptr);
 
 bool i2c_soft_read(i2c_soft_device* device_ptr, uint8_t* buff_ptr, uint8_t max_size);
-bool i2c_soft_write(i2c_soft_device* device_ptr, uint8_t* buff_ptr, uint8_t max_size);
+bool i2c_soft_write(i2c_soft_device* device_ptr, uint8_t* buff_ptr, uint8_t size);
 
 #endif // I2C_SOFT_LIB_H
---
+++ src/i2c_soft_lib.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include "../include/i2c_soft_lib.h"
+
+static uint8_t i2c_soft_byte_read(i2c_soft_bus* bus_ptr);
+static uint8_t i2c_soft_byte_write(i2c_soft_bus* bus_ptr);
+
+void i2c_soft_init(i2c_soft_bus* bus_ptr, i2c_soft_init_struct* init_ptr){
+  ASSERT(bus_ptr);
+  ASSERT(init_ptr);
+
+  bus_ptr->read_scl_ptr = init_ptr->read_scl_ptr;
+  bus_ptr->read_sda_ptr = init_ptr->read_sda_ptr;
+
+}
+
+void i2C_soft_handler(i2c_soft_bus* bus_ptr){
+
+}
+
+bool i2c_soft_read(i2c_soft_device* device_ptr, uint8_t* buff_ptr, uint8_t max_size){
+
+}
+
+bool i2c_soft_write(i2c_soft_device* device_ptr, uint8_t* buff_ptr, uint8_t size){
+
+}
+
+static uint8_t i2c_soft_byte_read(i2c_soft_bus* bus_ptr){
+
+}
+
+static uint8_t i2c_soft_byte_write(i2c_soft_bus* bus_ptr){
+
+}
