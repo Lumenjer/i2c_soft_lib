8fb87679c55a89e8e251d463074a4cab20b9ae6e
Lumenjer <lumenjer@gmail.com>
2022-12-20 01:21:56 +0200

    Add transmission stop, add wait until, fix bugs

--- src/i2c_soft_lib.cpp
+++ src/i2c_soft_lib.cpp
@@ -1,7 +1,11 @@
 #include <Arduino.h>
 #include "../include/i2c_soft_lib.h"
 
-#define MAX_ADDRESS (127)
+#define MAX_ADDRESS             (127)
+#define I2C_ADDRESS_START       (0x08)      // firs available address I2C (0x00 - 0x07 are reserved)
+#define I2C_ADDRESS_END         (0x77)      // last available address I2C (0x78 - 0x7F are reserved)
+#define I2C_SOFT_TIMEOUT_US     (1000)      // timeout for correct line condition, range 1 - 65535
+#define I2C_DELAY_1_US          (1)         // TODO:
 
 typedef enum {
   I2C_SOFT_READ   = 1u,
@@ -11,7 +15,8 @@ typedef enum {
 static bool i2c_soft_byte_read(i2c_soft_bus* bus_ptr, uint8_t* byte);
 static bool i2c_soft_byte_write(i2c_soft_bus* bus_ptr, uint8_t byte);
 static bool i2c_soft_transmission_start(i2c_soft_device* dev_ptr);
-
+static bool i2c_soft_transmission_stop(i2c_soft_device* dev_ptr);
+inline static bool i2c_soft_wait_until_line(i2c_soft_delay_micros_cb delay_micros, i2c_soft_read_pin_cb read_pin, bool condition);
 
 ///=========================================================================
 /// @brief @todo
@@ -54,7 +59,7 @@ uint8_t i2c_soft_device_lookup(i2c_soft_bus* bus_ptr, i2c_soft_device* dev_arr){
 
   uint8_t device_founded = 0;
 
-  for (uint8_t adr = 0; adr < MAX_ADDRESS; adr++){
+  for (uint8_t addr = I2C_ADDRESS_START; addr <= I2C_ADDRESS_END; addr++){
 
   }
 
@@ -81,24 +86,22 @@ bool i2c_soft_read(i2c_soft_device* device_ptr, uint8_t* buff_ptr, uint8_t max_s
     }
     else {
       result = false;
-      LOG(printf, "Transmit byte failed: idx = %d\n", idx);
+      // TODO:
       break;
     }
   }
 
-  bus->delay_micros(2);
-  bus->pull_scl_ptr(false);
-  bus->delay_micros(4);
-  bus->pull_sda_ptr(true);
-  bus->delay_micros(4);
-  bus->pull_scl_ptr(true);
+  if (result)
+  {
+    result = i2c_soft_transmission_start(device_ptr);
+  }
 
   if (result)
   {
     result = i2c_soft_byte_write(bus, (uint8_t)((device_ptr->addr << 1u) + I2C_SOFT_READ));
   }
   else {
-    LOG(println, "Address send failed");
+    // TODO:
   }
 
   if (result)
@@ -106,30 +109,19 @@ bool i2c_soft_read(i2c_soft_device* device_ptr, uint8_t* buff_ptr, uint8_t max_s
     for (uint8_t idx = 0; idx < max_size; idx++)
     {
       result = i2c_soft_byte_read(bus, &buff_ptr[idx]);
+      bus->pull_sda_ptr(idx < (max_size - 1));     // send NACK if last byte
       bus->pull_scl_ptr(false);
-      bus->delay_micros(1);
-      uint8_t timeout = 10;
-      while (--timeout && !bus->read_scl_ptr()){
-        bus->delay_micros(1);
-      }
-      if (timeout == 0)
-      {
-        // TODO:
-      }
-      bus->pull_sda_ptr(idx < max_size - 1);
+      bus->delay_micros(4);
+      i2c_soft_wait_until_line(bus->delay_micros, bus->read_scl_ptr, true);
+      bus->pull_scl_ptr(true);
+      bus->delay_micros(4);
     }
   }
   else {
-    LOG(println, "Addres send failed");
+    // TODO:
   }
 
-  bus->delay_micros(2);
-  bus->pull_sda_ptr(true);
-  bus->delay_micros(2);
-  bus->pull_scl_ptr(false);
-  bus->delay_micros(4);
-  bus->pull_sda_ptr(false);
-  bus->delay_micros(4);
+  i2c_soft_transmission_stop(device_ptr);
 
   uint32_t micros2 = micros();
 
@@ -157,17 +149,11 @@ static bool i2c_soft_byte_read(i2c_soft_bus* bus_ptr, uint8_t* byte){
   for (uint8_t bit_nums = BITS_IN_BYTE; bit_nums > 0; bit_nums--) {
     bus_ptr->pull_scl_ptr(false);
     bus_ptr->delay_micros(4);
+    i2c_soft_wait_until_line(bus_ptr->delay_micros, bus_ptr->read_scl_ptr, true);
     *byte |= (bus_ptr->read_sda_ptr() << bit_nums - 1);
     bus_ptr->pull_scl_ptr(true);
     bus_ptr->delay_micros(4);
-    uint8_t timeout = 10;
-    while (--timeout && !bus_ptr->read_scl_ptr())
-    {
-      bus_ptr->delay_micros(1);
-    }
-    if (timeout == 0) {
-      // TODO:
-    }
+    i2c_soft_wait_until_line(bus_ptr->delay_micros, bus_ptr->read_scl_ptr, false);
   }
 
   return result;
@@ -186,7 +172,6 @@ static bool i2c_soft_transmission_start(i2c_soft_device* dev_ptr){
   bus->delay_micros(4);
 
   if (bus->read_scl_ptr() == false || bus->read_sda_ptr() == false) {
-    // ASSERT(0);
     LOG(printf, "SCL = %d, SDA = %d\n", bus->read_scl_ptr(), bus->read_sda_ptr());
     return false;
   }
@@ -201,6 +186,54 @@ static bool i2c_soft_transmission_start(i2c_soft_device* dev_ptr){
   return result;
 }
 
+///=========================================================================
+/// @brief @todo
+///=========================================================================
+static bool i2c_soft_transmission_stop(i2c_soft_device* dev_ptr)
+{
+  bool result = false;
+  i2c_soft_bus* bus = dev_ptr->bus_ptr;
+
+  if (bus->read_scl_ptr())
+  {
+    bus->pull_scl_ptr(true);
+    i2c_soft_wait_until_line(bus->delay_micros, bus->read_scl_ptr, false);
+  }
+
+  if (bus->read_sda_ptr())
+  {
+    bus->pull_sda_ptr(true);
+    i2c_soft_wait_until_line(bus->delay_micros, bus->read_sda_ptr, false);
+  }
+
+  bus->pull_scl_ptr(false);
+  bus->delay_micros(4);
+  bus->pull_sda_ptr(false);
+  bus->delay_micros(4);
+
+  return result;
+}
+
+
+///=========================================================================
+/// @brief @todo
+///=========================================================================
+inline static bool i2c_soft_wait_until_line(i2c_soft_delay_micros_cb delay_micros, i2c_soft_read_pin_cb read_pin, bool condition)
+{
+  uint16_t timeout = I2C_SOFT_TIMEOUT_US;
+
+  while (--timeout && read_pin() != condition)
+  {
+    delay_micros(I2C_DELAY_1_US);
+  }
+
+  if (timeout)
+  {
+    return true;
+  }
+
+  return false;
+}
 
 ///=========================================================================
 /// @brief @todo
@@ -214,33 +247,18 @@ static bool i2c_soft_byte_write(i2c_soft_bus* bus_ptr, uint8_t byte){
     bus_ptr->delay_micros(2);
     bus_ptr->pull_scl_ptr(false);
     bus_ptr->delay_micros(4);
-    uint8_t timeout = 10;
-    while (--timeout)
-    {
-      if (bus_ptr->read_scl_ptr() == true &&  // slave can hold scl until data will obtained
-          bus_ptr->read_scl_ptr() != pull) {
-            break;
-          }
-    }
-    if (timeout == 0)
-    {
-      // TODO: Add error
-    }
+
+    i2c_soft_wait_until_line(bus_ptr->delay_micros, bus_ptr->read_scl_ptr, true);
+
     bus_ptr->pull_scl_ptr(true);
     bus_ptr->delay_micros(2);
   }
 
   bus_ptr->pull_sda_ptr(false);
   bus_ptr->pull_scl_ptr(false);
-  bus_ptr->delay_micros(3);
+  bus_ptr->delay_micros(4);
   result = !bus_ptr->read_sda_ptr();
   bus_ptr->pull_scl_ptr(true);
-
-  uint8_t timeout = 100;
-  while (timeout-- && !bus_ptr->read_sda_ptr()) // wait until bus will free
-  {
-    bus_ptr->delay_micros(1);
-  }
 
   return result;
 }
